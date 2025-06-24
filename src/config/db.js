c460bc72d5506c7539f7274564ba4cd97c09e0cc
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-06-24 09:34:32 +0600

    server up and running

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
